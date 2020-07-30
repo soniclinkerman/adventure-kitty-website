92d9e9bed46d32c21c8f0246ad5ff72b0efa9414
sonic <sonic@Isaiahs-MacBook-Pro.local>
2020-07-30 09:15:03 -0400

    Updates

---
+++ precache-manifest.0ab1305979ab81e765bcc2c932f76661.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36304730cfb8080f41c566e015128c01",
+    "url": "/adventure-kitty-website/index.html"
+  },
+  {
+    "revision": "24941023a2c25da93391",
+    "url": "/adventure-kitty-website/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f6d205f10e7d71c4cfee",
+    "url": "/adventure-kitty-website/static/css/main.29b933e2.chunk.css"
+  },
+  {
+    "revision": "24941023a2c25da93391",
+    "url": "/adventure-kitty-website/static/js/2.b06c035e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/adventure-kitty-website/static/js/2.b06c035e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6d205f10e7d71c4cfee",
+    "url": "/adventure-kitty-website/static/js/main.02d516a1.chunk.js"
+  },
+  {
+    "revision": "7898cdd482453b40fe71",
+    "url": "/adventure-kitty-website/static/js/runtime-main.a2e72177.js"
+  },
+  {
+    "revision": "a74ca6a391130e9a37c84c13db958f87",
+    "url": "/adventure-kitty-website/static/media/Anna.a74ca6a3.png"
+  },
+  {
+    "revision": "b10f565c79deebeb5e39abd398b00112",
+    "url": "/adventure-kitty-website/static/media/Anna_nocolor.b10f565c.png"
+  },
+  {
+    "revision": "71a306b178c7153a26d400f1572b109a",
+    "url": "/adventure-kitty-website/static/media/Carton.71a306b1.png"
+  },
+  {
+    "revision": "2d84c4df481904b4881efea5faeebb48",
+    "url": "/adventure-kitty-website/static/media/Carton_nocolor.2d84c4df.png"
+  },
+  {
+    "revision": "f875ae98a455c61dd9fb4bb27f6e4acc",
+    "url": "/adventure-kitty-website/static/media/Evil_Kitty.f875ae98.png"
+  },
+  {
+    "revision": "b7dd029c58c1f9ed094244f6a78e1618",
+    "url": "/adventure-kitty-website/static/media/Evil_Kitty_nocolor.b7dd029c.png"
+  },
+  {
+    "revision": "f3fed2d85793250e701ce42de25d7381",
+    "url": "/adventure-kitty-website/static/media/Felicity.f3fed2d8.png"
+  },
+  {
+    "revision": "db98474cbc14dadffbf164d682f276c6",
+    "url": "/adventure-kitty-website/static/media/Felicity_nocolor.db98474c.png"
+  },
+  {
+    "revision": "a6f50e2f9476cc749649398838ea373c",
+    "url": "/adventure-kitty-website/static/media/no_name.a6f50e2f.png"
+  },
+  {
+    "revision": "5102eace50779607488592d5db1455da",
+    "url": "/adventure-kitty-website/static/media/no_name_nocolor.5102eace.png"
+  }
+]);
